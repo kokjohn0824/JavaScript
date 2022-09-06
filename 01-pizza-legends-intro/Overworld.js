ce0919c18c2ee11b8031bdc30ab7be774ee0c65c
kokjohn <AlexChang@gmail.com>
2022-09-06 22:47:48 +0800

    all

--- 01-pizza-legends-intro/Overworld.js
+++ 01-pizza-legends-intro/Overworld.js
@@ -3,48 +3,32 @@ class Overworld {
     this.element = config.element;
     this.canvas = this.element.querySelector(".game-canvas");
     this.ctx = this.canvas.getContext("2d");
+    this.map = null;
+  }
+
+  startGameLoop() {
+    const step = () => {
+      //Draw Lower layer
+      this.map.drawLowerImage(this.ctx);
+
+      //Draw Game Objects
+      Object.values(this.map.gameObjects).forEach((object) => {
+        object.x += 0.02;
+        object.sprite.draw(this.ctx);
+      });
+
+      //Draw Upper layer
+      this.map.drawUpperImage(this.ctx);
+
+      requestAnimationFrame(() => {
+        step();
+      });
+    };
+    step();
   }
 
   init() {
-    const image = new Image();
-    image.onload = () => {
-      this.ctx.drawImage(image, 0, 0);
-    };
-    image.src = "/images/maps/DemoLower.png";
-
-    const x = 5;
-    const y = 6;
-
-    const shadow = new Image();
-    shadow.onload = () => {
-      this.ctx.drawImage(
-        shadow,
-        0, //left cut
-        0, //top cut,
-        32, //width of cut
-        32, //height of cut
-        x * 16 - 8,
-        y * 16 - 18,
-        32,
-        32
-      );
-    };
-    shadow.src = "/images/characters/shadow.png";
-
-    const hero = new Image();
-    hero.onload = () => {
-      this.ctx.drawImage(
-        hero,
-        0, //left cut
-        0, //top cut,
-        32, //width of cut
-        32, //height of cut
-        x * 16 - 8,
-        y * 16 - 18,
-        32,
-        32
-      );
-    };
-    hero.src = "/images/characters/people/hero.png";
+    this.map = new OverworldMap(window.OverworldMaps.Kitchen);
+    this.startGameLoop();
   }
 }
